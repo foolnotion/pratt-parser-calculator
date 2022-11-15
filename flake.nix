61da8b0581a73a55784f2fa4f8a48b9b95252a76
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-11-15 17:53:59 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,33 +1,36 @@
 {
   description = "pratt-parser dev";
-  nixConfig.bash-prompt = "\n\\[\\e[93m\\e[1m\\][pratt-parser-dev:\\[\\e[92m\\e[1m\\]\\w]$\\[\\e[0m\\] ";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nur.url = "github:nix-community/NUR";
+  inputs.foolnotion.url = "github:foolnotion/nur-pkg";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
 
-  outputs = { self, flake-utils, nixpkgs, nur }:
+  outputs = { self, flake-utils, nixpkgs, foolnotion }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [ nur.overlay ];
+            overlays = [ foolnotion.overlay ];
           };
         in rec
         {
-          defaultPackage = pkgs.gcc11Stdenv.mkDerivation {
+          packages.default = pkgs.stdenv.mkDerivation {
             name = "pratt-parser";
             src = self;
             nativeBuildInputs = with pkgs; [ cmake ];
+
             buildInputs = with pkgs; [
-                pkgs.nur.repos.foolnotion.fast_float
                 doctest
+                fast_float
               ];
           };
 
-          devShell = pkgs.gcc11Stdenv.mkDerivation {
+          devShells.default = pkgs.gcc11Stdenv.mkDerivation {
             name = "pratt-parser-dev";
+
+            nativeBuildInputs = packages.default.nativeBuildInputs;
+            buildInputs = packages.default.buildInputs;
           };
         }
       );
